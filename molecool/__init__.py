8123a102d60d72e18d23ec5a46883f58b076b2ce
Heta Gandhi <hgandhi@ur.rochester.edu>
2020-02-04 10:21:17 -0500

    Initial commit, forgot to commit earlier

---
+++ molecool/__init__.py
@@ -0,0 +1,15 @@
+"""
+molecool
+A python package for the MSF 2020A bootcamp
+"""
+
+# Add imports here
+from .functions import *
+from .atom_data import atomic_weights, atom_colors
+
+# Handle versioneer
+from ._version import get_versions
+versions = get_versions()
+__version__ = versions['version']
+__git_revision__ = versions['full-revisionid']
+del get_versions, versions
